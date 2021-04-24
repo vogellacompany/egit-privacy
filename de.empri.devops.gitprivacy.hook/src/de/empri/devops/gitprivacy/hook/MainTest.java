52183fe7c35326807db0b3ab5d66b486f3974fe4
fap <fapdash@users.noreply.github.com>
2021-04-24 22:22:49 +0200

    [Hook] Adjust production path
    Working dir of a git hook is the top level directory of the repository.

--- de.empri.devops.gitprivacy.hook/src/de/empri/devops/gitprivacy/hook/MainTest.java
+++ de.empri.devops.gitprivacy.hook/src/de/empri/devops/gitprivacy/hook/MainTest.java
@@ -21,13 +21,14 @@ public class MainTest {
 			if (args.length > 0 && args[0].equals("-dev")) {
 				repoPath = System.getProperty("user.home") + "/git/hooktesting/.git/";
 			} else {
-				repoPath = "../";
+				repoPath = ".";
 			}
 			git = Git.open(new File(repoPath));
 			Repository repository = git.getRepository();
 			ObjectId lastCommitId = repository.resolve(Constants.HEAD);
 			RevWalk revWalk = new RevWalk(repository);
 			RevCommit lastCommit = revWalk.parseCommit(lastCommitId);
+			revWalk.close();
 			PersonIdent authorIdent = lastCommit.getAuthorIdent();
 			PersonIdent committerIdent = lastCommit.getCommitterIdent();
 			Date date = new Date(0);
@@ -48,54 +49,3 @@ public class MainTest {
 		}
 	}
 }
-
-//package de.empri.devops.gitprivacy.hook;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//import org.eclipse.jgit.api.Git;
-//import org.eclipse.jgit.api.errors.GitAPIException;
-//import org.eclipse.jgit.lib.Constants;
-//import org.eclipse.jgit.lib.ObjectId;
-//import org.eclipse.jgit.lib.PersonIdent;
-//import org.eclipse.jgit.lib.Repository;
-//import org.eclipse.jgit.revwalk.RevCommit;
-//import org.eclipse.jgit.revwalk.RevWalk;
-//
-//import de.empri.devops.gitprivacy.hook.ProvidesCommitDate.CommitDateResult;
-//
-//public class MainTest {
-//	public static void main(String[] args) {
-//		Git git;
-//		try {
-//			git = Git.open(new File(System.getProperty("user.home") + "/git/hooktesting/.git/"));
-//			Repository repository = git.getRepository();
-//
-//			ProvidesCommitDate providesCommitDate = new ProvidesCommitDate();
-//			CommitDateResult commitDateResult = providesCommitDate.commitDate(repository.getConfig());
-//			if (commitDateResult.isRedated()) {
-//				ObjectId lastCommitId = repository.resolve(Constants.HEAD);
-//				RevWalk revWalk = new RevWalk(repository);
-//				RevCommit lastCommit = revWalk.parseCommit(lastCommitId);
-//				PersonIdent authorIdent = lastCommit.getAuthorIdent();
-//				PersonIdent committerIdent = lastCommit.getCommitterIdent();
-//				RevCommit revCommit = null;
-//				try {
-//					revCommit = git.commit()
-//							.setAmend(true)
-//							.setMessage(lastCommit.getFullMessage())
-//							.setAuthor(new PersonIdent(authorIdent, commitDateResult.getCommitDate()))
-//							.setAuthor(new PersonIdent(committerIdent, commitDateResult.getCommitDate()))
-//							.call();
-//				} catch (GitAPIException e) {
-//					e.printStackTrace();
-//				}
-//				System.out.println(revCommit);
-//			}
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//}
