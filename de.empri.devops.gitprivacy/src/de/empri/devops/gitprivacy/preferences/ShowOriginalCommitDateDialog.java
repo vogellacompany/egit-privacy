ea2f02ac4060a27bcf0bbd93d5f36ee9efa082b5
fap <fapdash@users.noreply.github.com>
2021-04-28 19:34:55 +0200

    [Settings] add redacted timestamps to dialog

--- de.empri.devops.gitprivacy/src/de/empri/devops/gitprivacy/preferences/ShowOriginalCommitDateDialog.java
+++ de.empri.devops.gitprivacy/src/de/empri/devops/gitprivacy/preferences/ShowOriginalCommitDateDialog.java
@@ -43,26 +43,44 @@ public class ShowOriginalCommitDateDialog extends Dialog {
 		Composite main = new Composite(parent, SWT.NONE);
 		GridLayoutFactory.swtDefaults().applyTo(main);
 		GridDataFactory.fillDefaults().indent(0, 0).grab(true, true).applyTo(main);
+
 		Group commitDateGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
-		commitDateGroup.setText("Original Commit Date");
+		commitDateGroup.setText("Commit Date");
 		GridLayoutFactory.fillDefaults().applyTo(commitDateGroup);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(commitDateGroup);
 		GridLayoutFactory.swtDefaults().applyTo(commitDateGroup);
 		Label commitDate = new Label(commitDateGroup, SWT.NONE);
+		commitDate.setText(commit.getCommitterIdent().getWhen().toString());
+
+		Group realCommitDateGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		realCommitDateGroup.setText("Real Commit Date");
+		GridLayoutFactory.fillDefaults().applyTo(realCommitDateGroup);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(realCommitDateGroup);
+		GridLayoutFactory.swtDefaults().applyTo(realCommitDateGroup);
+		Label realCommitDate = new Label(realCommitDateGroup, SWT.NONE);
+
 		Group authorDateGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
-		authorDateGroup.setText("Original Author Date");
+		authorDateGroup.setText("Real Commit Date");
 		GridLayoutFactory.fillDefaults().applyTo(authorDateGroup);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(authorDateGroup);
 		GridLayoutFactory.swtDefaults().applyTo(authorDateGroup);
 		Label authorDate = new Label(authorDateGroup, SWT.NONE);
+		authorDate.setText(commit.getAuthorIdent().getWhen().toString());
+
+		Group realAuthorDateGroup = new Group(main, SWT.SHADOW_ETCHED_IN);
+		realAuthorDateGroup.setText("Real Author Date");
+		GridLayoutFactory.fillDefaults().applyTo(realAuthorDateGroup);
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(realAuthorDateGroup);
+		GridLayoutFactory.swtDefaults().applyTo(realAuthorDateGroup);
+		Label realAuthorDate = new Label(realAuthorDateGroup, SWT.NONE);
 
 		List<String> allKeys = new ManagesKeyStorage(repository.getDirectory()).readAllKeys();
 		if (!allKeys.isEmpty()) {
 			OriginalCommitDateEncoder encoder = new OriginalCommitDateEncoder(new Crypto(allKeys));
 			Optional<DecodedDates> decode = encoder.decode(commit.getFullMessage());
 			if (decode.isPresent()) {
-				commitDate.setText(decode.get().getCommittedDateTime().toString());
-				authorDate.setText(decode.get().getAuthoredDateTime().toString());
+				realCommitDate.setText(decode.get().getCommittedDateTime().toString());
+				realAuthorDate.setText(decode.get().getAuthoredDateTime().toString());
 			}
 		}
 
