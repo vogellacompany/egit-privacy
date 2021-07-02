b403082f9a0daa4f7179614692418402895258a0
fap <fapdash@users.noreply.github.com>
2021-07-02 18:00:54 +0200

    FIX: PropertyTester not working
    Had to force the plugin loading.
    Solution found here: https://stackoverflow.com/a/27238958/4266296
    
    fixes #10

--- de.empri.devops.gitprivacy/src/de/empri/devops/gitprivacy/preferences/CommitHasEncryptedDatesPropertyTester.java
+++ de.empri.devops.gitprivacy/src/de/empri/devops/gitprivacy/preferences/CommitHasEncryptedDatesPropertyTester.java
@@ -1,12 +1,32 @@
 package de.empri.devops.gitprivacy.preferences;
 
+import java.util.Optional;
+
 import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.egit.core.internal.IRepositoryCommit;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+import de.empri.devops.gitprivacy.preferences.shared.OriginalCommitDateEncoder;
+import de.empri.devops.gitprivacy.preferences.shared.OriginalCommitDateEncoderFactory;
+import de.empri.devops.gitprivacy.preferences.shared.OriginalCommitDateEncoder.DecodedDates;
 
 public class CommitHasEncryptedDatesPropertyTester extends PropertyTester {
 
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
-		System.out.println("###### HELLO FROM PROPERTY TESTER");
+		if (!(receiver instanceof IRepositoryCommit)) {
+			return false;
+		}
+		
+		IRepositoryCommit commit = (IRepositoryCommit) receiver;
+		Optional<OriginalCommitDateEncoder> encoderOptional = OriginalCommitDateEncoderFactory.build(commit.getRepository());
+		if (encoderOptional.isPresent()) {
+			RevCommit revCommit = commit.getRevCommit();
+			Optional<DecodedDates> decode = encoderOptional.get().decode(revCommit.getFullMessage());
+			if (decode.isPresent()) {
+				return true;
+			}
+		}
 		return false;
 	}
 
