4c5d16e539616ef90026eef853b5060a9ae1507b
fap <fapdash@users.noreply.github.com>
2021-09-21 21:02:59 +0200

    Add config initialization to initialization process

--- de.empri.devops.gitprivacy/src/de/empri/devops/gitprivacy/preferences/UIPreferences.java
+++ de.empri.devops.gitprivacy/src/de/empri/devops/gitprivacy/preferences/UIPreferences.java
@@ -12,4 +12,22 @@ public class UIPreferences {
 
 	public static final String GitPrivacyPage_GitConfigPrivacySection_Salt = "salt";
 
+	public static final String MODIFY_COMMIT_DATE = "modifyCommitState";
+
+	public static final String MODIFY_COMMIT_MONTH = "modifyCommitMonth";
+
+	public static final String MODIFY_COMMIT_DAY = "modifyCommitDay	";
+
+	public static final String MODIFY_COMMIT_HOUR = "modifyCommitHour";
+
+	public static final String MODIFY_COMMIT_MINUTE = "modifyCommitMinute";
+
+	public static final String MODIFY_COMMIT_SECOND = "modifyCommitSecond";
+
+	public static final String LIMIT_COMMIT_TIME = "limitCommitTime";
+
+	public static final String LOWER_COMMIT_TIME_LIMIT = "lowerCommitTimeLimit";
+
+	public static final String UPPER_COMMIT_TIME_LIMIT = "upperCommitTimeLimit";
+
 }
